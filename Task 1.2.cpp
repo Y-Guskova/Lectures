fbae5c7e313b563e5bd7ab32dfae45060d12a949
Y-Guskova <86667524+Y-Guskova@users.noreply.github.com>
2021-09-07 12:16:32 +0300

    Add files via upload

---
+++ Task 1.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+#include <string>
+using namespace std;
+
+int main()
+{
+	char c;
+	int a, b;
+	cout << "Enter first letter of your name: ";
+	cin >> c;
+	a = c - '0';
+	b = a % 2 + 1;
+	cout << "Your option is: " << b;
+	return 0;
+}
