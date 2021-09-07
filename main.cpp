083576147e083ba76108d3b376ed7813b8bf79bd
Y-Guskova <86667524+Y-Guskova@users.noreply.github.com>
2021-09-07 14:26:20 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "struril.h"
+using namespace std;
+
+int main()
+{
+    char a[40], str[100];
+    cout << "Enter first string: " << endl;
+    cin >> str;
+    cout << "Enter string to find: " << endl;
+    cin >> a;  
+    return 0;
+}
---
+++ strutil.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int strConteins(char* a, char* str)
+{
+	int pos = -1;
+	int i, j ;
+	for (int i = 0; i < s.lenght(str); i++)
+	{
+		if (str[i] == a[0])
+		{
+			pos = i;
+			for (int j = 0; j < s.lenght(a); j++)
+			{
+				i++
+				{
+					if (str[i] != a[j])
+					{
+						pos = -1
+						break
+					}
+				}
+			}
+		}
+		i++;
+		if (pos != -1)
+		break;
+	}
+	return -1;
+}
