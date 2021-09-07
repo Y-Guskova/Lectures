7eb1ce3effd0bd53df363ea0744b6270ea8cbbaa
Y-Guskova <86667524+Y-Guskova@users.noreply.github.com>
2021-09-07 12:19:38 +0300

    Add files via upload

---
+++ Task 1.1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream> 
+using namespace std;
+
+int main()
+{
+	int Num, a;
+	cout << "Enter your list number: ";
+	cin >> Num;
+	a = Num % 2 + 1;
+	cout << "Your option is: " << a << endl;
+	return 0;
+}
